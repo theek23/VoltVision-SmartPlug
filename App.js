fe7cc092e4708cee7c9833b2e2e3131025e0f742
theek23 <theekshanadesilva28@gmail.com>
2024-06-06 12:39:18 +0530

    button for on off

--- App.js
+++ App.js
@@ -1,23 +1,58 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet, TouchableOpacity, Image } from 'react-native';
 
 const App = () => {
   const handlePress = async () => {
     try {
-      const response = await fetch('http://192.168.8.100/off');
-      const data = await response.json();
-      console.log('Response:', data);
+      const response = await fetch('http://192.168.8.100/');
+      console.log(response);
     } catch (error) {
       console.error('Error:', error);
     }
   };
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>VOLTVISION</Text>
-      <Button title="On/Off" onPress={handlePress} />
+    <View style={styles.container}>
+      <Image source={require('./asset/imgs/logo.png')} style={styles.logo} resizeMode="contain"/>
+      <Text style={styles.title}>For Better Home</Text>
+      <TouchableOpacity style={styles.button} onPress={handlePress}>
+        <Text style={styles.buttonText}>On/Off</Text>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f4f7',
+  },
+  logo: {
+    width: 300,
+    height: 100,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#333',
+  },
+  button: {
+    backgroundColor: '#6200ee',
+    width: 100,
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 50, 
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
 
 export default App;
